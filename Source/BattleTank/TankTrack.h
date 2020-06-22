0eb07170966ee6f3e1773c40412b35f48696c623
Artur Symanovic <artur.symanovic@gmail.com>
2020-06-22 16:29:48 +0100

    Added TankTrack class

---
+++ Source/BattleTank/TankTrack.h
@@ -0,0 +1,17 @@
+// Copyright Artur Symanovic 2020
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "TankTrack.generated.h"
+
+/**
+ * 
+ */
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class BATTLETANK_API UTankTrack : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+};
