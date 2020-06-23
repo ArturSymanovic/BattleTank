7aac5cf8730378dd1d1230b0efeff29619546142
Artur Symanovic <artur.symanovic@gmail.com>
2020-06-23 15:59:37 +0100

    Tweeked movement basics

---
+++ Source/BattleTank/TankMovementComponent.h
@@ -0,0 +1,17 @@
+// Copyright Artur Symanovic 2020
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/NavMovementComponent.h"
+#include "TankMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API UTankMovementComponent : public UNavMovementComponent
+{
+	GENERATED_BODY()
+	
+};
--- Source/BattleTank/TankTrack.cpp
+++ Source/BattleTank/TankTrack.cpp
@@ -9,5 +9,6 @@ void UTankTrack::SetThrottle(float Throttle)
 	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
 	auto ForceLocation = GetComponentLocation();
 	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
-	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
+	//TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
+	Cast<UPrimitiveComponent>(this)->AddForceAtLocation(ForceApplied, ForceLocation);
 }
